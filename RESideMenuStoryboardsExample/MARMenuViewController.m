5a6cc1a0c476ae70899131cdbf4cafc071bb7e3c
simara001 <aramis001@gmail.com>
2014-05-23 21:12:48 -0700

    Removing unnecessary logs, changing the color of the UINavigationBar and adding URLs for reference

--- RESideMenuStoryboardsExample/MARMenuViewController.m
+++ RESideMenuStoryboardsExample/MARMenuViewController.m
@@ -113,22 +113,18 @@
 
 - (void)sideMenu:(RESideMenu *)sideMenu willShowMenuViewController:(UIViewController *)menuViewController
 {
-    NSLog(@"willShowMenuViewController");
 }
 
 - (void)sideMenu:(RESideMenu *)sideMenu didShowMenuViewController:(UIViewController *)menuViewController
 {
-    NSLog(@"didShowMenuViewController");
 }
 
 - (void)sideMenu:(RESideMenu *)sideMenu willHideMenuViewController:(UIViewController *)menuViewController
 {
-    NSLog(@"willHideMenuViewController");
 }
 
 - (void)sideMenu:(RESideMenu *)sideMenu didHideMenuViewController:(UIViewController *)menuViewController
 {
-    NSLog(@"didHideMenuViewController");
 }
 
 @end
